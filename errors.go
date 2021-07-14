c8a18511087204cf7ee65f1df4e588379c4d5f15
Thunder33345 <Thunder33345@users.noreply.github.com>
2021-07-14 22:24:59 +0800

    docs: docs for error

--- errors.go
+++ errors.go
@@ -4,8 +4,15 @@ import "fmt"
 
 var _ error = (*MissingGroupsError)(nil) // ensure MissingGroupsError implements error
 
+//MissingGroupsError Is an error raised by Process when group provider fails to load a certain groups
 type MissingGroupsError struct {
 	groups []string
+}
+
+func NewMissingGroupsError(groups []string) MissingGroupsError {
+	return MissingGroupsError{
+		groups: groups,
+	}
 }
 
 func (e MissingGroupsError) Error() string {
