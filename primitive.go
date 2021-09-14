3d37f67a17f91e85b1ffe28ceb08bfbc84a345f2
Thunder33345 <Thunder33345@users.noreply.github.com>
2021-09-14 08:13:36 +0800

    change: added json tags to primitive.go

--- primitive.go
+++ primitive.go
@@ -3,33 +3,33 @@ package roller
 //Group represent a collection of permissions and metadata
 type Group struct {
 	//Name is a display friendly name of the group, it should only be used for display, it has no filtering or requirement
-	Name string
+	Name string `json:"name"`
 	//RefName is a command friendly runtime stable name, should be unique
-	RefName string
+	RefName string `json:"ref_name"`
 	//ID is the unique identifier for this group used for saving and referencing, must never be changed
-	ID string
+	ID string `json:"id"`
 	//Weight dictates the overwriting precedent, where the larger overwrites the smaller
 	//must be unique, otherwise behaviour is undefined
-	Weight int
+	Weight int `json:"weight"`
 	//Permission is the permission that is used
-	Permission Entry
+	Permission Entry `json:"permission,omitempty"`
 	//Flags are conditional Entry that only applies in certain situations
-	Flags map[string]FlagEntry
+	Flags map[string]FlagEntry `json:"flags,omitempty"`
 }
 
 //Entry represent a collection of permissions and flags
 type Entry struct {
 	//EmptySet will discard all previously granted permissions
-	EmptySet bool
+	EmptySet bool `json:"empty_set,omitempty"`
 	//Level is the default power level of said entry
 	//Only the highest group's level is in used
-	Level int
+	Level int `json:"level,omitempty"`
 	//SetLevel makes overwrites the Level instead of adding or subtracting from last level
-	SetLevel bool
+	SetLevel bool `json:"set_level,omitempty"`
 	//Grant will add permissions to the List
-	Grant []string
+	Grant []string `json:"grant,omitempty"`
 	//Revoke will revoke a permissions that is granted to the List by a prior group
-	Revoke []string
+	Revoke []string `json:"revoke,omitempty"`
 }
 
 //FlagEntry is an Entry but inside a Group.Flags
@@ -37,9 +37,9 @@ type Entry struct {
 type FlagEntry struct {
 	//Weight dictates the overwriting precedent, must be unique, otherwise behaviour is undefined
 	//behaviour is defined by processor
-	Weight int
+	Weight int `json:"weight"`
 	//Preprocess indicates that this should be processed before Group.Permission
-	Preprocess bool
+	Preprocess bool `json:"preprocess,omitempty"`
 	Entry
 }
 
@@ -47,17 +47,17 @@ type FlagEntry struct {
 type RawList struct {
 	//Overwrites has the highest precedent
 	//Will overwrite all group based permissions
-	Overwrites Entry
+	Overwrites Entry `json:"overwrites,omitempty"`
 	//Groups are a list of group UUID to inherit permission from
-	Groups []string
+	Groups []string `json:"groups,omitempty"`
 	//Flags are conditional overwrites for said raw list
-	Flags map[string]FlagEntry
+	Flags map[string]FlagEntry `json:"flags,omitempty"`
 }
 
 //List is the compiled result from a RawList
 type List struct {
 	//Level is the final applicable level
-	Level int
+	Level int `json:"level,omitempty"`
 	//Permission is th final applicable permission
-	Permission []string
+	Permission []string `json:"permission,omitempty"`
 }
